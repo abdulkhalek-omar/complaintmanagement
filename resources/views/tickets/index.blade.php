43f5f05e0256f3d95f8bd8f1fd477b7f82f50b93
OmarAk <omar.abdulkhalek@edu.fhdw.de>
2022-05-11 05:17:42 +0200

    error Message

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="">
             {{ __('Tickets Management') }}
@@ -11,6 +10,51 @@
         {{ __('Submit a Complaint') }}
     </a>
 
+    <section class="mb-5">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                            <li><a class="dropdown-item active" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+
+
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">Dropdown Example
+                            <span class="caret"></span></button>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
+                            <li role="presentation" class="divider"></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 
     <section class="bg-light shadow-sm">
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,26 +8,33 @@
     <div class="container bg-white pt-3 pb-3">
         <div class="row">
             <div class="col-12">
+
+                <x-jet-validation-errors class="mb-3"/>
+
                 <form method="post" action="{{ route('users.update', $user->id) }}">
                     @csrf
                     @method('put')
                     <div class="row g-3">
                         <div class="col-sm-12">
-                            <label for="username" class="{{ $errors->has('username') ? 'is-invalid' : '' }}">Username</label>
-                            <input type="text" name="username" id="username" class="form-control"
-                                   value="{{ old('username', $user->username) }}"/>
+                            <x-jet-label value="{{ __('Username') }}"/>
+                            <x-jet-input class="{{ $errors->has('username') ? 'is-invalid' : '' }}" type="text"
+                                         name="username"
+                                         value="{{ old('username', $user->username) }}" required autofocus
+                                         autocomplete="username"/>
                             <x-jet-input-error for="username"></x-jet-input-error>
                         </div>
 
                         <div class="col-sm-12">
-                            <label for="email" class="{{ $errors->has('email') ? 'is-invalid' : '' }}">Email</label>
-                            <input type="email" name="email" id="email" class="form-control"
-                                   value="{{ old('email', $user->email) }}"/>
+                            <x-jet-label value="{{ __('Email') }}"/>
+                            <x-jet-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email"
+                                         name="email"
+                                         value="{{ old('email', $user->email) }}" required/>
                             <x-jet-input-error for="email"></x-jet-input-error>
                         </div>
 
                         <div class="col-sm-12">
-                            <label for="password" class="{{ $errors->has('password') ? 'is-invalid' : '' }}">Password</label>
+                            <label for="password"
+                                   class="{{ $errors->has('password') ? 'is-invalid' : '' }}">Password</label>
                             <input type="password" name="password" id="password" class="form-control"/>
                             <x-jet-input-error for="password"></x-jet-input-error>
                         </div>
@@ -42,9 +49,9 @@
                                     </option>
                                 @endforeach
                             </select>
-                            @error('roles')
-                            <p class="">{{ $message }}</p>
-                            @enderror
+                            <x-jet-input-error for="roles" style="display: contents"></x-jet-input-error>
+
+
                         </div>
 
                         <div class="col-sm-12">
