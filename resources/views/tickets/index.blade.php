914a6627ebc797ca58ad88a48c4178116a512b15
OmarAk <omar.abdulkhalek@edu.fhdw.de>
2022-05-14 23:46:17 +0200

    Frontend Clean Code

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -5,70 +5,43 @@
         </h2>
     </x-slot>
 
-    <a href="{{ route('tickets.create') }}"
-       class="btn btn-outline-success mb-3">
-        {{ __('Submit a Complaint') }}
-    </a>
 
-    <section class="mb-5">
-        <div class="container">
-            <div class="row">
+    <div class="container bg-white pt-3 pb-3 mb-5">
+        <div class="row">
 
-                <div class="col-lg-4">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                            <li><a class="dropdown-item active" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
-                    </div>
-                </div>
+            <div class="col-lg-4">
+                <a href="{{ route('tickets.create') }}"
+                   class="btn btn-outline-success mb-3">
+                    {{ __('Submit a Complaint') }}
+                </a>
+            </div>
 
-                <div class="col-lg-4">
-
-
-                    <div class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">Dropdown Example
-                            <span class="caret"></span></button>
-                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
-                            <li role="presentation" class="divider"></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
-                        </ul>
-                    </div>
-
-
-                </div>
+            <div class="col-lg-4">
 
             </div>
-        </div>
-    </section>
 
-
-    <section class="bg-light shadow-sm">
-        <div class="container">
-            <div class="row">
-                @foreach($cards as $card)
-                    @if($card->closed)
-                        <x-ticket-card :card="$card"/>
-                    @else
-                        <x-ticket-card :card="$card"/>
-                    @endif
-                @endforeach
+            <div class="col-lg-4">
+                <a href="{{ route('tickets.create') }}"
+                   class="btn btn-outline-success mb-3">
+                    {{ __('Submit a Complaint') }}
+                </a>
             </div>
+
+
         </div>
-    </section>
+    </div>
+
+
+    <div class="container bg-light">
+        <div class="row">
+            @foreach($cards as $card)
+                @if($card->closed)
+                    <x-ticket-card :card="$card"/>
+                @else
+                    <x-ticket-card :card="$card"/>
+                @endif
+            @endforeach
+        </div>
+    </div>
 
 </x-app-layout>
