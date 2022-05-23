b0928c71672b0e81e5060091ad611d809cfbead2
OmarAbdulkhalek <omar.abdulkhalek@edu.fhdw.de>
2022-05-23 22:09:09 +0200

    clean Code (more Clean Code is needed)

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -25,13 +25,151 @@
         </div>
     @endcan
 
+
     <div class="container bg-light">
         <div class="row">
             @foreach($cards as $card)
+
                 @if($card->closed)
-                    <x-ticket-card :card="$card"/>
+                    @php
+                        $card_color               = "text-white bg-success";
+                        $btn_color                = "btn btn-outline-dark";
+                        $footer_color             = "text-info";
+                        $btn_text                 = "Open Ticket";
+                    @endphp
+
+                    @canany(['employee_access', 'admin_access'])
+                        <div class="col-lg-4 mb-5 d-flex align-items-stretch">
+                            <div class="card {{$card_color}}">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">{{__('Created by')}}
+                                        : {{ $card->customer->surname }} {{ $card->customer->firstname }}</h5>
+                                    <p class="card-text mb-4">{!! $card->ticket->content !!}</p>
+
+                                    <form action="{{ route('tickets.openTicket') }}" method="POST">
+                                        @csrf
+                                        <input name="id" value="{{$card->id}}" hidden/>
+                                        <button type="submit"
+                                                class="{{$btn_color . ' mt-auto align-self-start form-control'}}">{{ __($btn_text) }}</button>
+                                    </form>
+
+                                    @can('admin_access')
+
+                                        <form
+                                            action="{{ route('tickets.assign.index', ['employee_id' => $card->employee->id, 'id' => $card->id]) }}"
+                                            method="GET">
+                                            @csrf
+                                            <button type="submit" class="btn btn-outline-light mb-1 mt-2 form-control">
+                                                {{ __('Assigning the ticket to a Employee or another Employee') }}
+                                            </button>
+                                        </form>
+                                    @endcan
+
+                                </div>
+                                <div class="card-footer {{$footer_color}}">
+                                    <div>{{__('Assigned to')}}
+                                        : {{ $card->employee->surname }} {{ $card->employee->firstname }}</div>
+                                    {{__('Assigned at')}}: {{ $card->assignment_at->diffForHumans() }} <br>
+                                    {{__('Expiry at')}}: {{ $card->expiry_at->diffForHumans($card->assignment_at) }}
+                                </div>
+                            </div>
+                        </div>
+
+                    @elsecan('user_access')
+                        <div class="col-lg-4 mb-5 d-flex align-items-stretch">
+                            <div class="card {{$card_color}}">
+                                <div class="card-body d-flex flex-column">
+                                    {{--                <h5 class="card-title">{{__('Created by')}}: {{ $card->customer->surname }} {{ $card->customer->firstname }}</h5>--}}
+                                    <p class="card-text mb-4">{!! $card->ticket->content !!}</p>
+
+
+                                    <form action=" {{ route('tickets.satisfied.store') }} " method="POST">
+                                        @csrf
+                                        <input name="satisfied" value="0" hidden/>
+                                        <input name="id" value="{{ $card->id }}" hidden/>
+                                        <button type="submit" class="form-control btn btn-outline-dark mb-1">
+                                            {{ __('Satisfied') }}
+                                        </button>
+                                    </form>
+
+
+                                    <form action=" {{ route('tickets.satisfied.index', ['id' => $card->id]) }} "
+                                          method="GET">
+                                        @csrf
+                                        <button type="submit" class="form-control btn btn-outline-danger">
+                                            {{ __('Not Satisfied') }}
+                                        </button>
+                                    </form>
+
+                                </div>
+                                <div class="card-footer {{$footer_color}}">
+                                    {{__($card->response)}}
+                                    <div class="mt-2">{{__('Answer from Employee')}}
+                                        : {{ $card->employee->surname }} {{ $card->employee->firstname }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    @endcan
+
                 @else
-                    <x-ticket-card :card="$card"/>
+                    @php
+                        $card_color               = "text-white bg-dark";
+                        $btn_color                = "btn btn-outline-secondary";
+                        $footer_color             = "text-muted";
+                        $btn_text                 = "Close Ticket";
+                    @endphp
+
+                    @canany(['employee_access', 'admin_access'])
+                        <div class="col-lg-4 mb-5 d-flex align-items-stretch">
+                            <div class="card {{$card_color}}">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">{{__('Created by')}}
+                                        : {{ $card->customer->surname }} {{ $card->customer->firstname }}</h5>
+                                    <p class="card-text mb-4">{!! $card->ticket->content !!}</p>
+
+                                    <form action="{{ route('tickets.closeTicket.index', ['id' => $card->id]) }}"
+                                          method="GET">
+                                        @csrf
+                                        <button type="submit"
+                                                class="{{$btn_color . ' mt-auto align-self-start form-control'}}">{{ __($btn_text) }}</button>
+                                    </form>
+
+                                    @can('admin_access')
+                                        <form
+                                            action="{{ route('tickets.assign.index', ['employee_id' => $card->employee->id, 'id' => $card->id]) }}"
+                                            method="GET">
+                                            @csrf
+                                            <button type="submit" class="btn btn-outline-light mb-1 mt-2 form-control">
+                                                {{ __('Assigning the ticket to a Employee or another Employee') }}
+                                            </button>
+                                        </form>
+                                    @endcan
+
+                                </div>
+                                <div class="card-footer {{$footer_color}}">
+                                    <div>{{__('Assigned to')}}
+                                        : {{ $card->employee->surname }} {{ $card->employee->firstname }}</div>
+                                    {{__('Assigned at')}}: {{ $card->assignment_at->diffForHumans() }} <br>
+                                    {{__('Expiry at')}}: {{ $card->expiry_at->diffForHumans($card->assignment_at) }}
+                                </div>
+                            </div>
+                        </div>
+
+                    @elsecan('user_access')
+                        <div class="col-lg-4 mb-5 d-flex align-items-stretch">
+                            <div class="card {{$card_color}}">
+                                <div class="card-body d-flex flex-column">
+                                    <p class="card-text mb-4">{!! $card->ticket->content !!}</p>
+                                </div>
+                                <div class="card-footer {{$footer_color}}">
+                                    <div>{{__('Assigned to')}}
+                                        : {{ $card->employee->surname }} {{ $card->employee->firstname }}</div>
+                                    {{__('Assigned at')}}: {{ $card->assignment_at->diffForHumans() }} <br>
+                                    {{__('Expiry at')}}: {{ $card->expiry_at->diffForHumans($card->assignment_at) }}
+                                </div>
+                            </div>
+                        </div>
+                    @endcan
                 @endif
             @endforeach
         </div>
